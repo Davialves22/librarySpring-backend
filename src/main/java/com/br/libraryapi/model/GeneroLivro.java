c38b1b91ddb05f43c09e2997279a839ebade9a99
davi gerson <dgas1@discente.ifpe.edu.br>
2025-04-11 14:09:48 -0300

    mapeamento da entidade livro com lombok

---
+++ src/main/java/com/br/libraryapi/model/GeneroLivro.java
@@ -0,0 +1,6 @@
+package com.br.libraryapi.model;
+
+public enum GeneroLivro {
+    FICCAO, ROMANCE, DRAMA,
+    COMEDIA, FANTASIA, TERROR, DOCUMENTARIO, BIOGRAFIA;
+}
---
+++ src/main/java/com/br/libraryapi/model/Livro.java
@@ -0,0 +1,45 @@
+package com.br.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+//mapeamento de entidades JPA
+
+@Entity
+@Table(name = "livro") //nao e obrigatorio
+@Data
+
+public class Livro {
+
+    @Id
+    @Column(name = "id")// nao e obrigatorio
+    @GeneratedValue(strategy = GenerationType.UUID) //id gerado automaticamente
+    private UUID id;
+
+    @Column(name = "isbn", length = 20)
+    private String isbn;
+
+    @Column(name = "titulo", length = 150)
+    private String titulo;
+
+    @Column(name = "data_publicacao", nullable = false)
+    private LocalDate dataPublicacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genero", length = 30, nullable = false)
+    private GeneroLivro genero;
+
+    @Column(name = "preco", precision = 18, scale = 2)
+    private Double preco;
+    //private BigDecimal preco;
+
+    //mapeamento com relacionamento
+    @ManyToOne //muitos livros para um autor
+    @JoinColumn(name = "id_autor")
+    private Autor autor;
+}
