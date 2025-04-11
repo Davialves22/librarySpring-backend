966bef2b4807a820b071cbde3bcd894a262da7c9
davi gerson <dgas1@discente.ifpe.edu.br>
2025-04-11 14:27:56 -0300

    implementando os repositorios

---
+++ src/main/java/com/br/libraryapi/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package com.br.libraryapi.repository;
+
+import com.br.libraryapi.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
+}
---
+++ src/main/java/com/br/libraryapi/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.br.libraryapi.repository;
+
+import com.br.libraryapi.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LivroRepository extends JpaRepository<Livro, UUID> {
+}
