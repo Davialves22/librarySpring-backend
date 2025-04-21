81cd64f29a7e85cf8b04a76149997ebe450697d7
davi gerson <dgas1@discente.ifpe.edu.br>
2025-04-21 20:43:54 -0300

    trabalhando com relacionamentos

---
+++ src/test/java/com/br/libraryapi/repository/LivroRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.br.libraryapi.repository;
+
+import com.br.libraryapi.model.Autor;
+import com.br.libraryapi.model.GeneroLivro;
+import com.br.libraryapi.model.Livro;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@SpringBootTest
+class LivroRepositoryTest {
+    @Autowired
+    LivroRepository repository;
+
+    @Autowired
+    AutorRepository autorRepository;
+
+    @Test
+    void salvarTest() {
+        Livro livro = new Livro();
+        livro.setIsbn("9780545069670");
+        livro.setPreco(BigDecimal.valueOf(39, 93));
+        livro.setGenero(GeneroLivro.FANTASIA);
+        livro.setTitulo("Harry Potter e a Pedra Filosofal");
+        livro.setDataPublicacao(LocalDate.of(1997, 6, 26));
+
+        Autor autor = autorRepository
+                .findById(UUID.fromString("acf61694-43ec-46a5-b186-89573ade3fe8"))
+                .orElse(null);
+
+        livro.setAutor(autor);
+
+        repository.save(livro);
+    }
+}
