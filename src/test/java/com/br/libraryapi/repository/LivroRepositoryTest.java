defe74ce25a31dd84311fecad9831f767bf69397
davi gerson <dgas1@discente.ifpe.edu.br>
2025-05-19 12:54:22 -0300

    adicionado testes

--- src/test/java/com/br/libraryapi/repository/LivroRepositoryTest.java
+++ src/test/java/com/br/libraryapi/repository/LivroRepositoryTest.java
@@ -6,10 +6,13 @@ import com.br.libraryapi.modelo.livro.LivroRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LivroRepositoryTest {
 
     @Autowired
